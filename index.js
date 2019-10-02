d9a7e12b5c04b28531cc0cc07932bc258fc3fa9e
Michelle Sharpe <hidibyms@gmail.com>
2019-10-02 17:38:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,52 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(name){
+  
+}
+
+function destructivelyPrependKitten(name){
+  
+}
+
+function destructivelyRemoveLastKitten(){
+  
+}
+
+function destructivelyRemoveFirstKitten(){
+  
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
