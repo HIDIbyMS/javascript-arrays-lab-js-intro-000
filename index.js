188b5e01798ef577884483f039ed592c1fc893ca
Michelle Sharpe <hidibyms@gmail.com>
2019-10-02 17:47:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
+// .push() adds elements to end of array - mutates array
 function destructivelyAppendKitten(name){
-  
+  name.push();
 }
 
+// .unshift() adds elements to beginning of array - mutates array
 function destructivelyPrependKitten(name){
-  
+  name.unshift();
 }
 
 function destructivelyRemoveLastKitten(){
