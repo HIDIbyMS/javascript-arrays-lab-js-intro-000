782ca5e80d2f1363a62b94b94f2e94d642424feb
Michelle Sharpe <hidibyms@gmail.com>
2019-10-02 18:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
 
+// ******
 // .push() adds elements to end of array - mutates array
 function destructivelyAppendKitten(name){
   name.push();
   return name;
 }
 
+// ******
 // .unshift() adds elements to beginning of array - mutates array
 function destructivelyPrependKitten(name){
   name.unshift();
@@ -24,24 +26,29 @@ function destructivelyRemoveFirstKitten(){
   kittens.shift();
 }
 
+// ******
 // ["", ...kittens] adds to beginning without mutations
 function appendKitten(name) {
   [name, ...kittens];
 }
 
+// ******
 // [...kittens, ""] adds to end without mutations
 function prependKitten(name) {
   [...kittens, name];
 }
 
+// ******
 // .slice(0, kittens.length -1) removes last element
 function removeLastKitten() {
   name.slice(0, kittens.length -1);
 }
 
+// ******
 // .slice(1) removes element from the beginning of the array
 function removeFirstKitten() {
   name.slice(1);
+  return name;
 }
 
 
